6604031f7671e656364b9e80e82a285a0749ad12
chaitanya-goutam-9 <0112cs191035@gmail.com>
2025-12-20 21:05:00 +0530

    re import in router.jsx file , Home, profile

--- src/routes.jsx
+++ src/routes.jsx
@@ -13,7 +13,7 @@ import {
 } from "@heroicons/react/24/solid";
 import { SignIn, SignUp } from "@/pages/auth";
 // import AddPlan from "./pages/dashboard/AddPlan";
-import Home from "./pages/dashboard/Home";
+import Home from "./pages/dashboard/home";
 import Profile from "./pages/dashboard/Profile";
 import TestimonialAdmin from "./pages/dashboard/TestimonialAdmin";
 import Blog from "./pages/dashboard/Blog";
